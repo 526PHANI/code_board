9bc56e0b0d596c38ddd07c0bc07f4fbbb8fc40d6
LathasriGorli <lathasrigorli03@gmail.com>
2025-05-12 08:16:24 +0530

    UPDATE: View Profile

--- src/components/Profile/ViewProfile.tsx
+++ src/components/Profile/ViewProfile.tsx
@@ -4,22 +4,22 @@ import { Avatar, AvatarImage } from "../ui/avatar";
 
 export function ViewProfile() {
   return (
-    <Card className="w-(--an-profile-width) p-(--an-profile-padding) items-start rounded-(--an-profile-border-radius) bg-(--an-profile-background) m-5">
+    <Card className="w-300 h-45 p-4 items-start rounded-(--an-profile-border-radius) bg-(--an-profile-background) m-5 shadow-none border">
       <div className="flex">
-        <div className="pt-4">
-          <Avatar className="w-(--an-profile-avatar-size) h-(--an-profile-avatar-size) object-cover">
+        <div className="pt-2">
+          <Avatar className="w-35 h-35 object-cover">
             <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
           </Avatar>
         </div>
 
         <div className="flex flex-col">
           <CardHeader>
-            <div className="flex gap-(--an-profile-header-gap) items-start">
-              <CardTitle className="text-(--an-profile-text-color) font-urbanist text-(length:--an-profile-title-text-size) font-(--an-profile-text-weight)">
+            <div className="flex gap-5 items-start">
+              <CardTitle className="text-(--an-profile-text-color) font-[urbanist] text-(length:--an-profile-title-text-size) font-medium">
                 Riti Shan
               </CardTitle>
-              <div className="rounded-(--an-profile-active-border-radius) bg-(--an-profile-active-bg) flex justify-center items-center px-4 py-1 h-(--an-profile-active-height)">
-                <span className="text-(--an-profile-active-color) font-urbanist text-(length:--an-profile-active-text-size) font-(--an-profile-text-weight)">
+              <div className="rounded-4xl bg-(--an-profile-active-bg) flex justify-center items-center px-4 py-1 h-6">
+                <span className="text-(--an-profile-active-color) font-[urbanist] text-(length:--an-profile-active-text-size) font-medium">
                   Active
                 </span>
               </div>
@@ -27,76 +27,76 @@ export function ViewProfile() {
           </CardHeader>
 
           <CardContent className="mt-2">
-            <form className="flex flex-col items-start gap-(--an-profile-form-gap)">
-              <div className="flex flex-start gap-(--an-profile-form-div-gap) self-stretch">
-                <div className="flex flex-col gap-2 w-(--an-profile-form-div-width) justify-center align-start">
+            <form className="flex flex-col items-start gap-4">
+              <div className="flex flex-start gap-8 self-stretch">
+                <div className="flex flex-col gap-2 w-75 justify-center align-start">
                   <Label
                     htmlFor="email"
-                    className="text-(--an-profile-label-color) font-urbanist text-(length:--an-profile-text-size) font-(--an-profile-label-weight)"
+                    className="text-(--an-profile-label-color) font-[urbanist] text-(length:--an-profile-text-size) font-normal !important"
                   >
                     Email
                   </Label>
                   <p
                     id="email"
-                    className="text-(--an-profile-text-color) font-urbanist text-(length:--an-profile-text-size) font-(--an-profile-text-weight)"
+                    className="text-(--an-profile-text-color) font-[urbanist] text-(length:--an-profile-text-size) font-medium"
                   >
                     ritishan123@gmail.com
                   </p>
                 </div>
-                <div className="flex flex-col gap-2 w-(--an-profile-form-div-width) justify-center align-start">
+                <div className="flex flex-col gap-2 w-75 justify-center align-start">
                   <Label
                     htmlFor="mobile"
-                    className="text-(--an-profile-label-color) font-urbanist text-(length:--an-profile-text-size) font-(--an-profile-label-weight)"
+                    className="text-(--an-profile-label-color) font-[urbanist] text-(length:--an-profile-text-size) font-normal !important"
                   >
                     Mobile
                   </Label>
                   <p
                     id="mobile"
-                    className="text-(--an-profile-text-color) font-urbanist text-(length:--an-profile-text-size) font-(--an-profile-text-weight)"
+                    className="text-(--an-profile-text-color) font-[urbanist] text-(length:--an-profile-text-size) font-medium"
                   >
                     9123456789
                   </p>
                 </div>
-                <div className="flex flex-col gap-2 w-(--an-profile-form-div-width) justify-center align-start">
+                <div className="flex flex-col gap-2 w-75 justify-center align-start">
                   <Label
                     htmlFor="designation"
-                    className="text-(--an-profile-label-color) font-urbanist text-(length:--an-profile-text-size) font-(--an-profile-label-weight)"
+                    className="text-(--an-profile-label-color) font-[urbanist] text-(length:--an-profile-text-size) font-normal !important"
                   >
                     Designation
                   </Label>
                   <p
                     id="designation"
-                    className="text-(--an-profile-text-color) font-urbanist text-(length:--an-profile-text-size) font-(--an-profile-text-weight)"
+                    className="text-(--an-profile-text-color) font-[urbanist] text-(length:--an-profile-text-size) font-medium"
                   >
                     Frontend Developer
                   </p>
                 </div>
               </div>
-              <div className="flex flex-start gap-(--an-profile-form-div-gap) self-stretch">
-                <div className="flex flex-col gap-2 w-(--an-profile-form-div-width) justify-center align-start">
+              <div className="flex flex-start gap-8 self-stretch">
+                <div className="flex flex-col gap-2 w-75 justify-center align-start">
                   <Label
                     htmlFor="birth"
-                    className="text-(--an-profile-label-color) font-urbanist text-(length:--an-profile-text-size) font-(--an-profile-label-weight)"
+                    className="text-(--an-profile-label-color) font-[urbanist] text-(length:--an-profile-text-size) font-normal !important"
                   >
                     Date Of Birth
                   </Label>
                   <p
                     id="email"
-                    className="text-(--an-profile-text-color) font-urbanist text-(length:--an-profile-text-size) font-(--an-profile-text-weight)"
+                    className="text-(--an-profile-text-color) font-[urbanist] text-(length:--an-profile-text-size) font-medium"
                   >
                     26 March 2003
                   </p>
                 </div>
-                <div className="flex flex-col gap-2 w-(--an-profile-form-div-width) justify-center align-start">
+                <div className="flex flex-col gap-2 w-75 justify-center align-start">
                   <Label
                     htmlFor="joining"
-                    className="text-(--an-profile-label-color) font-urbanist text-(length:--an-profile-text-size) font-(--an-profile-label-weight)"
+                    className="text-(--an-profile-label-color) font-[urbanist] text-(length:--an-profile-text-size) font-normal !important"
                   >
                     Date Of Joining
                   </Label>
                   <p
                     id="mobile"
-                    className="text-(--an-profile-text-color) font-urbanist text-(length:--an-profile-text-size) font-(--an-profile-text-weight)"
+                    className="text-(--an-profile-text-color) font-[urbanist] text-(length:--an-profile-text-size) font-medium"
                   >
                     03 April 2024
                   </p>
